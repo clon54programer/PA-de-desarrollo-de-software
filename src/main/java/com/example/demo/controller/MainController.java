282aa4ad8f895d7af1633d20be2afb7cab6cca94
Clon54 <clon5454@outlook.com>
2025-11-18 15:37:56 -0500

    Agregue zonas para colocar el codigo.

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -11,4 +11,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MainController {
 
+    // begin of crud of actives
+
+    // end of crud of actives
+
+    // begin login
+
+    // end login
+
+    // begin sign up
+
+    // end sign up
+
+    // begin asignacion de vulnerabiliades y recomendaciones
+
+    // end asignacion de vulnerabiliades y recomendaciones
+
 }
