051af9ef2302d45b951ba268e9e22b4716d03cbc
Clon54 <clon5454@outlook.com>
2025-11-18 14:56:32 -0500

    Agregue los setters y getters a la clase.

--- src/main/java/com/example/demo/model/ActiveBase.java
+++ src/main/java/com/example/demo/model/ActiveBase.java
@@ -20,4 +20,36 @@ public abstract class ActiveBase {
     private Double valor;
 
     // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
 }
