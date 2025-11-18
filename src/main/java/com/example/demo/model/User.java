845ee64e14cde59d9b840d37d1a1deb57590aa20
Clon54 <clon5454@outlook.com>
2025-11-18 14:53:15 -0500

    Agregue el constructor, y los getters y setters de la clase.

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,5 @@
 package com.example.demo.model;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -11,8 +12,40 @@ public class User {
     @Column(length = 80, nullable = false)
     private String name;
 
+    
+
     @Column(length = 255, nullable = false) // almacenar hash de contraseña
     private String password;
 
+    public User(long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
     // Getters y Setters
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
