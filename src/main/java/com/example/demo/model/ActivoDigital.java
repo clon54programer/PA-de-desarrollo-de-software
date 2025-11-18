16ef8f47df0b61c538fb8efa4376c16d9bbc49b4
Clon54 <clon5454@outlook.com>
2025-11-18 15:04:59 -0500

    Agregue el constructor, y los setters y getters de la clase.

--- src/main/java/com/example/demo/model/ActivoDigital.java
+++ src/main/java/com/example/demo/model/ActivoDigital.java
@@ -12,5 +12,28 @@ public class ActivoDigital extends ActiveBase {
     @Column(length = 20)
     private String version;
 
+    public ActivoDigital(long id, String name, String descripcion, Double valor, String licencia, String version) {
+        super(id, name, descripcion, valor);
+        this.licencia = licencia;
+        this.version = version;
+    }
+
     // Getters y Setters
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
 }
