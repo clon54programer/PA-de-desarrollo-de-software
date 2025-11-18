58f71309d8f56142f77eab1a45fd3f9737da50bc
Clon54 <clon5454@outlook.com>
2025-11-18 15:15:38 -0500

    Agregue los getters y setters, y el constructor de la clase Vuln.

--- src/main/java/com/example/demo/model/Vuln.java
+++ src/main/java/com/example/demo/model/Vuln.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "vulnerabilidad")
@@ -36,5 +37,57 @@ public class Vuln {
     @JoinTable(name = "vulnerabilidad_recomendacion", joinColumns = @JoinColumn(name = "vulnerabilidad_id"), inverseJoinColumns = @JoinColumn(name = "recomendacion_id"))
     private List<Recomendacion> recomendaciones;
 
+    public Vuln(Long id, ActiveBase activoAfectado, String descripcion, String cve, EstadoVulnerabilidad estado,
+            LocalDate fechaDeDescubrimiento) {
+        this.activoAfectado = activoAfectado;
+        this.id = id;
+        this.descripcion = descripcion;
+        this.cve = cve;
+        this.fechaDeDescubrimiento = fechaDeDescubrimiento;
+        this.recomendaciones = new ArrayList<>();
+
+    }
+
     // Getters y Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ActiveBase getActivoAfectado() {
+        return activoAfectado;
+    }
+
+    public void setActivoAfectado(ActiveBase activoAfectado) {
+        this.activoAfectado = activoAfectado;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCve() {
+        return cve;
+    }
+
+    public void setCve(String cve) {
+        this.cve = cve;
+    }
+
+    public LocalDate getFechaDeDescubrimiento() {
+        return fechaDeDescubrimiento;
+    }
+
+    public void setFechaDeDescubrimiento(LocalDate fechaDeDescubrimiento) {
+        this.fechaDeDescubrimiento = fechaDeDescubrimiento;
+    }
+
 }
