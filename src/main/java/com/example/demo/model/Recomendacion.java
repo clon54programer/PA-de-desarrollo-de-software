3f8a2647032f479f2bfb114a4b6f6d56f726b1a4
Clon54 <clon5454@outlook.com>
2025-11-18 15:06:46 -0500

    Agregue el constructor, y los setters y getters de la clase.

--- src/main/java/com/example/demo/model/Recomendacion.java
+++ src/main/java/com/example/demo/model/Recomendacion.java
@@ -12,5 +12,25 @@ public class Recomendacion {
     @Column(columnDefinition = "TEXT")
     private String descripcion;
 
+    public Recomendacion(long id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
     // Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
