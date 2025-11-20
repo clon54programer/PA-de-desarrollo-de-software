e86762f5a9a8fe6e78ff04b086a8809cbd02eb73
Clon54 <clon5454@outlook.com>
2025-11-19 21:04:46 -0500

    Agregue una consulta personalizada.

--- src/main/java/com/example/demo/repository/ActivoBaseRepository.java
+++ src/main/java/com/example/demo/repository/ActivoBaseRepository.java
@@ -2,6 +2,9 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.ActiveBase;
+import java.util.List;
 
 public interface ActivoBaseRepository extends JpaRepository<ActiveBase, Long> {
+    List<ActiveBase> findByNombreContainingIgnoreCase(String nombre);
+
 }
