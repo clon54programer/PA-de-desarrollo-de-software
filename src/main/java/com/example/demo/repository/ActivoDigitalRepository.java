4d4d40c727784af8a315dfc48cb37d8fcf68c5f9
Clon54 <clon5454@outlook.com>
2025-11-20 14:51:20 -0500

    Agregue una consula personalizada.

--- src/main/java/com/example/demo/repository/ActivoDigitalRepository.java
+++ src/main/java/com/example/demo/repository/ActivoDigitalRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.model.ActivoDigital;
 
 public interface ActivoDigitalRepository extends JpaRepository<ActivoDigital, Long> {
+    List<ActivoDigital> findByNameContainingIgnoreCase(String name);
 }
