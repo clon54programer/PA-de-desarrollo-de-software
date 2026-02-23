bfb54a22aa6db00950afdd37efd75a83ae36c544
Clon54 <clon5454@outlook.com>
2026-02-23 12:32:59 -0500

    Agregue mi primera vista.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
 
 
-def index(request: HttpRequest):
+def index(request):
     return HttpResponse("Hello world")
