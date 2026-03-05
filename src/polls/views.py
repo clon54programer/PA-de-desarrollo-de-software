6cbf7c500bba20d6ed222595d1a6850d148df064
Clon54 <clon5454@outlook.com>
2026-03-04 21:11:09 -0500

    Agregue ua¿na nueva vista de prueba.

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Hello world")
+
+
+def prueba(request):
+    if request.user.is_authenticated:
+        return render(request,"polls/404.html")
+
+    return render(request,"polls/index.html")
