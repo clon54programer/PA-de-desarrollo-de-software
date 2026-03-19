0fa1f80385bb6ce10413e5834411de30a449686c
Clon54 <clon5454@outlook.com>
2026-03-18 19:10:30 -0500

    Agregue una nueva vista.

--- src/polls/views.py
+++ src/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -7,3 +8,8 @@ def index(request):
     if not request.user.is_authenticated:
         return render(request, "polls/404.html")
     return render(request, "polls/index.html", {"message": f"Hello world, {request.user.username}"})
+
+
+@login_required
+def init_scanner(request):
+    return render(request, "polls/scan")
