cdccd2e2f74e648c7dd1fbb53929aeca4e03cf7b
Clon54 <clon5454@outlook.com>
2026-05-06 17:49:41 -0500

    Agregue un nueva url en el proyecto.

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("scan", views.scanner, name="scan"),
     path("result", views.view_result, name="result"),
     path("service_result", views.get_services, name="service_result"),
+    path("pdf_generate", views.pdf_generate, name="pdf_generate"),
 ]
