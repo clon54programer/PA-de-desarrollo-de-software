c47f95daad37fbcbf246ace4dccc9153878de10b
Clon54 <clon5454@outlook.com>
2026-05-25 17:06:35 -0500

    Agregue las rutas para iniciar seccion y cerrar seccion.

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -2,10 +2,18 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("scan", views.scanner, name="scan"),
     path("result", views.view_result, name="result"),
     path("service_result", views.get_services, name="service_result"),
     path("pdf_generate", views.pdf_generate, name="pdf_generate"),
+    path(
+        "login",
+        auth_views.LoginView.as_view(template_name="polls/login.html"),
+        name="login",
+    ),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
 ]
